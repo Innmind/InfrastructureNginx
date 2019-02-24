4c01ea986be8043363960faf077152a8ab213f69
Baptiste Langlade <baptouuuu@gmail.com>
2019-02-24 18:29:37 +0100

    use os abstraction instead of building objects by hand

--- tests/BootstrapTest.php
+++ tests/BootstrapTest.php
@@ -6,13 +6,17 @@ namespace Tests\Innmind\Infrastructure\Nginx;
 use function Innmind\Infrastructure\Nginx\bootstrap;
 use Innmind\CLI\Commands;
 use Innmind\OperatingSystem\OperatingSystem;
+use Innmind\Url\Path;
 use PHPUnit\Framework\TestCase;
 
 class BootstrapTest extends TestCase
 {
     public function testBootstrap()
     {
-        $commands = bootstrap($this->createMock(OperatingSystem::class), '/tmp');
+        $commands = bootstrap(
+            $this->createMock(OperatingSystem::class),
+            new Path('/tmp')
+        );
 
         $this->assertInstanceOf(Commands::class, $commands);
     }
